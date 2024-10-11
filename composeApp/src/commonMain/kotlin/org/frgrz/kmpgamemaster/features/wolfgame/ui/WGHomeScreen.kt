c026e57b76357785729c5de4f7cbdeacd3ec9e19
François Grzybowski <64666213+frgrz-pro@users.noreply.github.com>
2024-10-11 21:12:45 +1300

    design roles selection screen

--- composeApp/src/commonMain/kotlin/org/frgrz/kmpgamemaster/features/wolfgame/ui/WGHomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/frgrz/kmpgamemaster/features/wolfgame/ui/WGHomeScreen.kt
@@ -1,40 +1,58 @@
 package org.frgrz.kmpgamemaster.features.wolfgame.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
-import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import androidx.constraintlayout.compose.ConstraintLayout
+import org.frgrz.kmpgamemaster.material.components.icons.IconPack
+import org.frgrz.kmpgamemaster.material.components.icons.Remove
 
 class WGHomeScreen : Screen {
+
+    class CardItem
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
@@ -42,86 +60,173 @@ class WGHomeScreen : Screen {
 
         val navigator = LocalNavigator.currentOrThrow
         val viewModel = getScreenModel<WGHomeViewModel>()
+        var selectedCardIndex by remember { mutableStateOf<Int?>(null) }
+
+        var dataList by remember {
+            mutableStateOf(
+                listOf(
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                    CardItem(),
+                )
+            )
+        }
 
         Scaffold(
             topBar = {
                 TopAppBar(title = { Text("Loup Garou") })
             },
-            content = { paddingValues ->
+            content = { _ ->
                 ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-                    val box = createRef()
+                    val column = createRef()
 
-                    Box(
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
                         modifier = Modifier
+                            .padding(32.dp)
                             .wrapContentSize()
-                            .constrainAs(box) {
+                            .constrainAs(column) {
                                 top.linkTo(parent.top)
                                 start.linkTo(parent.start)
                                 end.linkTo(parent.end)
                                 bottom.linkTo(parent.bottom)
                             }
-
                     ) {
-                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = viewModel.playerLabel.value,
+                            style = MaterialTheme.typography.headlineMedium
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Slider(
+                            value = viewModel.playerCount.value.toFloat(),
+                            onValueChange = { viewModel.onPlayerCountChanged(it.toInt()) },
+                            valueRange = viewModel.minPlayers.toFloat()..viewModel.maxPlayers.toFloat(),
+                            steps = viewModel.maxPlayers - viewModel.minPlayers,
+                            modifier = Modifier.width(240.dp)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            IconButton(
+                                onClick = { viewModel.onRemoveWolvesClicked() },
+                                enabled = viewModel.canRemoveWolves.value
+                            ) {
+                                val icon =
+                                    rememberVectorPainter(
+                                        image = IconPack.Remove
+                                    )
+                                Icon(
+                                    painter = icon,
+                                    contentDescription = "Remove"
+                                )
+                            }
+
                             Text(
-                                text = viewModel.playerLabel.value,
+                                text = viewModel.wolvesLabel.value,
+                                modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                                style = MaterialTheme.typography.titleLarge
+                            )
+
+                            IconButton(
+                                onClick = { viewModel.onAddWolvesClicked() },
+                                enabled = viewModel.canAddWolves.value
+                            ) {
+                                val icon =
+                                    rememberVectorPainter(
+                                        image = Icons.Filled.Add
+                                    )
+                                Icon(
+                                    painter = icon,
+                                    contentDescription = "Remove"
+                                )
+                            }
+                        }
+
+                        Spacer(modifier = Modifier.height(24.dp))
+
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text(
+                                text = "Rôles",
+                                modifier = Modifier.padding(end = 8.dp),
                                 style = MaterialTheme.typography.headlineMedium
                             )
-                            Spacer(modifier = Modifier.height(12.dp))
-                            Slider(
-                                value = viewModel.playerCount.value.toFloat(),
-                                onValueChange = { viewModel.onPlayerCountChanged(it.toInt()) },
-                                valueRange = viewModel.minPlayers.toFloat()..viewModel.maxPlayers.toFloat(),
-                                steps = viewModel.maxPlayers - viewModel.minPlayers,
-                                modifier = Modifier.width(240.dp)
+                        }
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(5),
+                            horizontalArrangement = Arrangement.spacedBy(4.dp),
+                            verticalArrangement = Arrangement.spacedBy(4.dp),
+                            modifier = Modifier.padding(12.dp)
+                        ) {
+                            itemsIndexed(dataList) { index, _ ->
+                                Card(
+                                    onClick = { selectedCardIndex = index },
+                                    elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+                                    shape = RoundedCornerShape(4.dp),
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .aspectRatio(1f)
+                                        .then(
+                                            if (index == selectedCardIndex) {
+                                                Modifier.border(
+                                                    2.dp,
+                                                    Color.Blue,
+                                                    RoundedCornerShape(4.dp)
+                                                )
+                                            } else {
+                                                Modifier
+                                            }
+                                        )
+                                ) {
+                                    Box(
+                                        modifier = Modifier.fillMaxSize()
+                                            .background(Color.Red)
+                                    )
+                                }
+                            }
+                        }
+
+                        TextButton(
+                            onClick = {},
+                        ) {
+                            Text(
+                                text = "Plus de rôles",
+                                modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                                style = MaterialTheme.typography.labelLarge
                             )
-                            Spacer(modifier = Modifier.height(8.dp))
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                            ) {
-                                SmallFloatingActionButton(
-                                    onClick = { viewModel.onRemoveWolvesClicked() }
-                                ) {}
-                                Text(
-                                    text = viewModel.wolvesLabel.value,
-                                    modifier = Modifier.padding(start = 8.dp, end = 8.dp),
-                                    style = MaterialTheme.typography.titleLarge
-                                )
-                                SmallFloatingActionButton(
-                                    onClick = { viewModel.onAddWolvesClicked() }
-                                ) {}
-                            }
-                            Spacer(modifier = Modifier.height(32.dp))
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Checkbox(
-                                    checked = viewModel.isSolosEnabled.value,
-                                    onCheckedChange = { viewModel.onSolosCheckedChanged(it) },
+                        }
 
-                                    )
-                                Text("Solos")
+                        Spacer(modifier = Modifier.height(48.dp))
 
-                                Checkbox(
-                                    checked = viewModel.isMayorEnabled.value,
-                                    onCheckedChange = { viewModel.onMayorCheckedChanged(it) },
-
-                                    )
-                                Text("Maire")
-                            }
-                            Spacer(modifier = Modifier.height(8.dp))
-                            TextButton(
-                                onClick = { navigator.push(WGRolesScreen()) }
-                            ) {
-                                Text("Choisir les rôles")
-                            }
-                            Spacer(modifier = Modifier.height(32.dp))
-                            FilledTonalButton(
-                                onClick = { }
-                            ) {
-                                Text("Lancer la partie")
-                            }
-
+                        FilledTonalButton(
+                            onClick = { },
+                            modifier = Modifier.fillMaxWidth()
+                                .padding(12.dp)
+                        ) {
+                            Text(
+                                text = "Lancer la partie",
+                                style = MaterialTheme.typography.titleMedium
+                            )
                         }
                     }
                 }
---
+++ composeApp/src/commonMain/kotlin/org/frgrz/kmpgamemaster/material/components/icons/IconPack.kt
@@ -0,0 +1,17 @@
+package org.frgrz.kmpgamemaster.material.components.icons
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import kotlin.collections.List as ____KtList
+
+object IconPack
+
+private var __Icons: ____KtList<ImageVector>? = null
+
+val IconPack.Icons: ____KtList<ImageVector>
+  get() {
+    if (__Icons != null) {
+      return __Icons!!
+    }
+    __Icons= listOf(Remove)
+    return __Icons!!
+  }
